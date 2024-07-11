b4e2e65cd431f462bc6cc5d9690d89dd5aa537d1
Syed Umer Ahmed <syedumerahmedcode@gmail.com>
2024-07-11 20:16:41 +0200

    Feature: My own solution as per my interpretation of the problem statement.

--- leetcodetop100solutions/src/com/umer/stack/MinStackDifficultyLevelMedium.java
+++ leetcodetop100solutions/src/com/umer/stack/MinStackDifficultyLevelMedium.java
@@ -1,19 +1,21 @@
 package com.umer.stack;
 
+import java.util.Stack;
+
 /**
- * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+* Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
 Implement the MinStack class:
 
-    MinStack() initializes the stack object.
-    void push(int val) pushes the element val onto the stack.
-    void pop() removes the element on the top of the stack.
-    int top() gets the top element of the stack.
-    int getMin() retrieves the minimum element in the stack.
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
 
 You must implement a solution with O(1) time complexity for each function.
 
- 
+
 
 Example 1:
 
@@ -34,35 +36,59 @@ minStack.pop();
 minStack.top();    // return 0
 minStack.getMin(); // return -2
 
- 
+
 
 Constraints:
 
-    -231 <= val <= 231 - 1
-    Methods pop, top and getMin operations will always be called on non-empty stacks.
-    At most 3 * 104 calls will be made to push, pop, top, and getMin.
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
 
 
- */
+*/
 public class MinStackDifficultyLevelMedium {
-
+    
+    Stack<Integer> stack=new Stack<>();
+    Stack<Integer> min=new Stack<>();
+    
     public MinStackDifficultyLevelMedium() {
         
     }
     
     public void push(int val) {
-        
+        stack.push(val);
+        if(min.empty()){
+            min.push(val);
+        }else{
+            if(val<min.peek()){
+                min.push(val);
+            }
+        }
     }
     
     public void pop() {
+        if(stack.peek()==min.peek()){
+            min.pop();
+        }
+        stack.pop();
+        // if (min>stack.peek()) {
+            //     min=stack.peek();
+            // }
+        }
         
-    }
-    
-    public int top() {
+        public int top() {
+            return stack.peek();
+        }
         
+        public int getMin() {
+            return min.peek();     
+        }
     }
-    
-    public int getMin() {
-        
-    }
-}
+    /**
+    * Your MinStack object will be instantiated and called as such:
+    * MinStack obj = new MinStack();
+    * obj.push(val);
+    * obj.pop();
+    * int param_3 = obj.top();
+    * int param_4 = obj.getMin();
+    */
--- leetcodetop100solutions/src/com/umer/stack/Stack.java
+++ leetcodetop100solutions/src/com/umer/stack/Stack.java
@@ -8,6 +8,19 @@ public class Stack {
         System.out.println("The following input should return true from the class: input--> ()[]{}: Expected: true::: Actual:"+     validParameters.isValid("()[]{}"));
         System.out.println("The following input should return true from the class: input--> (]: Expected: false::: Actual:"+     validParameters.isValid("(]"));
 
+        MinStackDifficultyLevelMedium minStack=new MinStackDifficultyLevelMedium();
+        System.out.println("minStack.push(-2);");
+        minStack.push(-2);
+        System.out.println("minStack.push(0);");
+        minStack.push(0);
+        System.out.println("minStack.push(-3);");
+        minStack.push(-3);
+        System.out.println("The following input should return true from the class: minStack.getMin(); // return -3"+minStack.getMin());
+        System.out.println("minStack.pop();");
+        minStack.pop();
+        System.out.println("The following input should return true from the class: minStack.top();    // return 0"+minStack.top());
+        System.out.println("The following input should return true from the class: minStack.getMin(); // return -2"+minStack.getMin());
+
     }
 
 }
